ceb6649a50d420c1b8bbe208a3f9da7b3c03e99a
David Rodriguez <rdavid1986@outlook.com>
2024-03-18 21:44:32 -0300

    first commit portfolio

---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+});
+async function loadContent(url) {
+    await fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector(".mainContainer").innerHTML = html;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+
+const about = document.getElementById("about");
+about.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("click on about ")
+    loadContent("./pages/about.html");
+})
+
+const portfolio = document.getElementById("portfolio");
+portfolio.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("click on portfolio ")
+    loadContent("./pages/portfolio.html");
+})
+const contact = document.getElementById("contact");
+contact.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("click on contact ")
+    loadContent("./pages/contact.html");
+})
